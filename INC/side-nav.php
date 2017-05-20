8799e7155c48b873a593592a49f92c88399c50c7
lnzd17 <lindsaykayedutton@gmail.com>
2017-05-20 19:00:18 -0400

    Bottom-corner

--- INC/side-nav.php
+++ INC/side-nav.php
@@ -1,11 +1,18 @@
 <div id="mySidenav" class="sidenav">
-  <h1>Contact Me</h1>
-  <img src="IMG/map.png" class='map'>
-  <div class="sidenav-links">
-    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-    <a href="#"><i class="fa fa-skype" aria-hidden="true"></i></i></a>
-    <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-    <a href="#"><i class="fa fa-mobile" aria-hidden="true"></i></a>
-    <a href="#"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+  <div class="push-down">
+
   </div>
+
+  <div class="bottom-corner">
+    <h1>Contact Me</h1>
+    <img src="IMG/map.png" class='map'>
+    <div class="sidenav-links">
+      <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+      <a href="#"><i class="fa fa-skype" aria-hidden="true"></i></i></a>
+      <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+      <a href="#"><i class="fa fa-mobile" aria-hidden="true"></i></a>
+      <a href="#"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+    </div>
+  </div>
+
 </div>
