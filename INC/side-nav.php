12c4f32f71f6d147434bbf66f2599724d645c41d
lnzd17 <lindsaykayedutton@gmail.com>
2017-05-20 19:09:53 -0400

    Added instagram back

--- INC/side-nav.php
+++ INC/side-nav.php
@@ -7,6 +7,7 @@
     <h1>Contact Me</h1>
     <img src="IMG/map.png" class='map'>
     <div class="sidenav-links">
+      <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
       <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
       <a href="#"><i class="fa fa-skype" aria-hidden="true"></i></i></a>
       <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
