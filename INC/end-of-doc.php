3b822b8fb8bf793d16840339155d2ff5b6c29540
lnzd17 <lindsaykayedutton@gmail.com>
2017-05-22 18:43:59 -0400

    Added map interaction

--- INC/end-of-doc.php
+++ INC/end-of-doc.php
@@ -2,7 +2,14 @@
   src="https://code.jquery.com/jquery-3.1.1.js"
   integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA="
   crossorigin="anonymous"></script>
+<script
+  src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"
+  integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30="
+  crossorigin="anonymous"></script>
 <script src="JS/mobile.js"></script>
+<script src="JS/map.js"></script>
+<script src="JS/ps.js"></script>
+<script src="https://use.fontawesome.com/cf39e837b4.js"></script>
 </div>
 </body>
 </html>
---
+++ JS/map.js
@@ -0,0 +1,44 @@
+
+
+$( "#dialog" ).dialog({
+  autoOpen: false,
+  modal: true,
+  width: 'auto',
+  height: 'auto',
+  closeText: "X"
+ });
+$( ".map" ).click(function() {
+  $( "#dialog" ).dialog( "open" );
+});
+
+function findCountry(){
+  var countries = ["MX","US","GH","AU","CO","PA","BR","GB","FR"];
+  $.each(countries, function(index, value){ return value})
+}
+
+$(window).click(function(e){
+  var country = e.target.className.baseVal;
+  var countries = ["MX","US","GH","AU","CO","PA","BR","GB","FR"];
+  var abbr = "";
+  var fullClass = "amcharts-map-area amcharts-map-area-";
+    $.each(countries, function(index, value){
+      if (country === fullClass + countries[index]) {
+        abbr = countries[index];
+      }
+    });
+    console.log(abbr);
+    $(".amcharts-map-area-" + abbr).css('fill','red');
+});
+
+
+
+
+// amcharts-map-area-MX
+// amcharts-map-area-US
+// amcharts-map-area-GH
+// amcharts-map-area-AU
+// amcharts-map-area-CO
+// amcharts-map-area-PA
+// amcharts-map-area-BR
+// amcharts-map-area-GB
+// amcharts-map-area-FR
